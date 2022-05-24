648b8c58ca4a0a5342c2a152171e8f0bd9b3b2de
Zach Fawcett'e <zgf83810@ucmo.edu>
2022-05-24 18:36:24 -0500

    Added Company CRUD And Repo

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,85 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                var companyObj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                return View(companyObj);
+            }
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                }
+
+                _unitOfWork.Save();
+                TempData["Success"] = "Company Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var companyFromDbFirst = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            if (companyFromDbFirst == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(companyFromDbFirst);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+
+        #endregion
+    }
+}
